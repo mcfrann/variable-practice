779144435d9a35b0630e439c9ed4d623b02485af
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-05 18:00:02 -0600

    Add objects corrected

--- model-1.js
+++ model-1.js
@@ -9,3 +9,11 @@ var hasRoommates = false
 
 var sisters = ["Amanda", "Sophia"]
 var parents = ["Rob", "Maria"]
+
+var joseph = {
+    ageYears: 27,
+    pets: "Ida",
+    favMovie: "Point Break"
+}
+
+console.log(joseph.pets);
